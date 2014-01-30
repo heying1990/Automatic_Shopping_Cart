486b3e8111442fe260ad3c3d735479c5e5018789
Ying He <yinghe@Yings-MacBook-Pro.local>
2014-01-30 00:48:15 -0500

    control motor

---
+++ Motor.ino
@@ -0,0 +1,17 @@
+int transistorPin = 5; // connect to transistor base 
+
+void setup() {                          
+  pinMode(transistorPin, OUTPUT);
+}
+
+void loop() {                           // read the potentiometer, convert it to between 0 - 255 for the value accepted by the digital pin.
+  analogWrite(transistorPin, 255);
+  delay(2000);
+  analogWrite(transistorPin, 127);
+  delay(2000);
+  analogWrite(transistorPin, 63);
+  delay(2000);
+  analogWrite(transistorPin, 0);
+  delay(2000);
+}
+
